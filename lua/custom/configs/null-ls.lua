c72737abd3be1928ac95e618d7c6d39eb9462806
Ronnel James Bote <ronneljames.miranda.bote@gmail.com>
2023-10-08 17:28:49 +0300

    Add autoformat on save
    * Add config on null_ls to format on save
    * Update formatter config for prettier

--- lua/custom/configs/null-ls.lua
+++ lua/custom/configs/null-ls.lua
@@ -5,17 +5,50 @@ local b = null_ls.builtins
 local sources = {
 
   -- webdev stuff
-  b.formatting.deno_fmt, -- choosed deno for ts/js files cuz its very fast!
-  b.formatting.prettier.with { filetypes = { "html", "markdown", "css" } }, -- so prettier works only on these filetypes
+  b.formatting.prettier.with {
+    filetypes = {
+      "javascript",
+      "javascriptreact",
+      "typescript",
+      "typescriptreact",
+      "vue",
+      "css",
+      "scss",
+      "less",
+      "html",
+      "json",
+      "jsonc",
+      "yaml",
+      "markdown",
+      "markdown.mdx",
+      "graphql",
+      "handlebars",
+    },
+    prefer_local = "node_modules/.bin",
+  },
 
   -- Lua
   b.formatting.stylua,
-
-  -- cpp
-  b.formatting.clang_format,
 }
 
+local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+
+local on_attach = function(client, bufnr)
+  if client.supports_method "textDocument/formatting" then
+    vim.api.nvim_clear_autocmds { group = augroup, buffer = bufnr }
+
+    vim.api.nvim_create_autocmd("BufWritePre", {
+      group = augroup,
+      buffer = bufnr,
+      callback = function()
+        vim.lsp.buf.format { async = false }
+      end,
+    })
+  end
+end
+
 null_ls.setup {
+  on_attach = on_attach,
   debug = true,
   sources = sources,
 }
