addd48e0c5a2737e97e36aa89718c4f7b483f4f8
Ronnel James Bote <ronneljames.miranda.bote@gmail.com>
2023-10-08 19:34:30 +0300

    Identify and highlight blade template files

--- init.lua
+++ init.lua
@@ -19,3 +19,22 @@ end
 dofile(vim.g.base46_cache .. "defaults")
 vim.opt.rtp:prepend(lazypath)
 require "plugins"
+
+local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
+parser_config.blade = {
+  install_info = {
+    url = "https://github.com/EmranMR/tree-sitter-blade",
+    files = { "src/parser.c" },
+    branch = "main",
+  },
+  filetype = "blade",
+}
+
+local bladeGrp = vim.api.nvim_create_augroup("BladeFiletypeRelated", { clear = true })
+vim.api.nvim_create_autocmd({ "BufNewFile", "BufRead" }, {
+  pattern = "*.blade.php",
+  group = bladeGrp,
+  callback = function()
+    vim.opt.filetype = "blade"
+  end,
+})
