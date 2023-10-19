4c576273f2f3a4b49907be1a71940e5ba8955457
Ronnel James Miranda Bote <ronneljames.miranda.bote@gmail.com>
2023-10-19 14:40:31 +0300

    Show file path in statusline

--- lua/custom/chadrc.lua
+++ lua/custom/chadrc.lua
@@ -1,3 +1,33 @@
+local utils = require "custom.utils"
+
+local function fileInfo()
+  local empty_string = "Empty "
+
+  local icon = " 󰈚 "
+  local project_path = empty_string
+
+  local fullpath = vim.api.nvim_buf_get_name(utils.stbufnr())
+  local filename = (fullpath == "" and empty_string) or fullpath:match "([^/\\]+)[/\\]*$"
+
+  if filename ~= empty_string then
+    local devicons_present, devicons = pcall(require, "nvim-web-devicons")
+
+    if devicons_present then
+      local ft_icon = devicons.get_icon(filename)
+
+      icon = (ft_icon ~= nil and " " .. ft_icon) or icon
+    end
+
+    project_path = utils.replace(fullpath, vim.loop.cwd() .. "/", "")
+
+    if project_path:len() > 120 then
+      project_path = "..." .. filename
+    end
+  end
+
+  return "%#StText# " .. icon .. " " .. project_path .. " "
+end
+
 local M = {}
 
 local highlights = require "custom.highlights"
@@ -6,6 +36,9 @@ M.ui = {
   theme = "onedark",
   statusline = {
     theme = "vscode_colored",
+    overriden_modules = function(modules)
+      modules[2] = fileInfo()
+    end,
   },
   hl_override = highlights.override,
   hl_add = highlights.add,
